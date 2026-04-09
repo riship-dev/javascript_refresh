4f757500c19ae902d03bb58ddc878463f35bc596
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2026-04-09 16:41:07 +0530

    refresh ternary operators

---
+++ 12_ternary_operator.js
@@ -0,0 +1,10 @@
+/*
+ternary operator:
+a shortcut to if{} and else{} statements
+helps assign variable based on condition
+condition ? code_if_true : code_if_false
+*/
+
+let age = 22
+is_eligible = age > 18 ? true : false
+console.log(is_eligible)

84c3aea821c36de49228576c0d39fa3a52de5dc6
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2026-04-09 15:25:36 +0530

    refresh variables

---
+++ 02_variables.js
@@ -0,0 +1,12 @@
+let x = 10
+let my_name = "John Doe"
+
+console.log(x)
+console.log(`My name is ${my_name}`) // use `` for f strings
+
+console.log(typeof x) // number
+console.log(typeof 10.99) // number
+console.log(typeof my_name) // string
+console.log(typeof true) // boolean
+
+document.getElementById("p1").textContent = my_name

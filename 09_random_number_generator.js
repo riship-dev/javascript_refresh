d01fc645bd264d6cf1b35cc5a8ec6521c90332f9
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2026-04-09 16:27:11 +0530

    refresh random numbers

---
+++ 09_random_number_generator.js
@@ -0,0 +1,7 @@
+let random_number = Math.random()
+
+console.log(random_number)
+
+console.log(Math.floor(random_number * 10)) // generates random number 0-9
+console.log(Math.floor(random_number * 7)) // generates random number 0-6
+console.log(Math.floor(random_number * 6) + 1) // generates random number 1-6

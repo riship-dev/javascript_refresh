21b9f7fa65474e0521d7f0192ef5432cb76dad39
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2026-04-09 16:20:20 +0530

    refresh Math object

---
+++ 08_math_object.js
@@ -0,0 +1,21 @@
+// Math: built-in object that provides a collection of properties and methods
+
+let x = 3;
+let y = 2;
+let z = 1;
+
+// console.log(Math.round(x));
+// console.log(Math.floor(x));
+// console.log(Math.ceil(x));
+// console.log(Math.trunc(x));
+// console.log(Math.pow(x, y));
+// console.log(Math.sqrt(x));
+// console.log(Math.log(x));
+// console.log(Math.sin(x));
+// console.log(Math.cos(x));
+// console.log(Math.tan(x));
+// console.log(Math.abs(x));
+console.log(Math.sign(x));
+// console.log(Math.PI);
+// console.log(Math.max(x, y, z));
+// console.log(Math.min(x, y, z));

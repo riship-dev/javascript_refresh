15be39aac58648bae29d4393ff00cb5f4fa89e7b
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2026-04-09 16:45:28 +0530

    refresh switches

---
+++ 13_switches.js
@@ -0,0 +1,27 @@
+let day = 7
+
+switch(day) {
+    case 1:
+        console.log("mon")
+        break
+    case 2:
+        console.log("tue")
+        break
+    case 3:
+        console.log("wed")
+        break
+    case 4:
+        console.log("thu")
+        break
+    case 5:
+        console.log("fri")
+        break
+    case 6:
+        console.log("sat")
+        break
+    case 7:
+        console.log("sun")
+        break
+    default:
+        console.log("invalid")
+}

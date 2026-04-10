c6b6bc743bdd8d624154f1603ffa4cdcb82a691a
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2026-04-10 11:42:04 +0530

    refresh functions

---
+++ 22_functions.js
@@ -0,0 +1,12 @@
+/* 
+function:
+a section of reusable code
+declare code once, use it whenever you want
+call function to execute the code
+*/
+
+function add(x, y){
+    return x + y
+}
+
+console.log(add(1, 2))

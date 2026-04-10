9ec4926757bdf1541e8c42b1a8fdf2cfccb1d89d
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2026-04-10 14:05:55 +0530

    refresh arrow functions

---
+++ 36_arrow_functions.js
@@ -0,0 +1,9 @@
+/* 
+arrow functions: 
+a concise way to write function expressions good for simple functions that you use only once 
+(parameters) => some code
+*/
+
+const hello = (my_name) => console.log(`Hello ${my_name}`)
+
+hello("John Doe")

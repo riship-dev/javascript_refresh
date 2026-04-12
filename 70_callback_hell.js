aa78f2da14458fc35e40cc087eb533316f964fd1
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2026-04-12 23:42:55 +0530

    refresh callback hell

---
+++ 70_callback_hell.js
@@ -0,0 +1,42 @@
+/*
+Callback Hell:
+Situation in JavaScript where callbacks are nested within other callbacks to the degree where the code is difficult to read.
+Old pattern to handle asynchronous functions.
+Use Promises + async/await to avoid Callback Hell
+*/
+const print = console.log
+
+function task1(callback){
+    setTimeout(() => { 
+        print("task 1 complete")
+        callback() 
+    }, 2000)
+}
+function task2(callback){
+    setTimeout(() => { 
+        print("task 2 complete")
+        callback()
+    }, 1000)
+}
+function task3(callback){
+    setTimeout(() => { 
+        print("task 3 complete")
+        callback()
+    }, 3000)
+}
+function task4(callback){
+    setTimeout(() => { 
+        print("task 4 complete")
+        callback()
+    }, 1500)
+}
+
+task1(() => {
+    task2(() => {
+        task3(() => {
+            task4(() => {
+                print("All tasks completed")
+            })
+        })
+    })
+})
